400a7fd3a1a1ff6db9d8817abfb4fb229f6baa60
Domenik Niemietz <domenikniemietz@Domeniks-MBP.fritz.box>
2019-11-07 15:15:05 +0100

    Added formula

---
+++ cmdg.rb
@@ -0,0 +1,24 @@
+class Cmdg < Formula
+  desc "Gmail CLI - Copyright Thomas Habets thomas@habets.se 2019"
+  homepage "https://github.com/ThomasHabets/cmdg"
+  url "https://github.com/ThomasHabets/cmdg/archive/cmdg-1.00.tar.gz"
+  sha256 "4a723a812eae27fac1334e14cfc9b92955e6a3c1c43f1de87aa294d72bb7dc25"
+  depends_on "go" => :build
+  def install
+    ENV["GOPATH"] = buildpath
+    system "go", "get", "github.com/mattn/go-runewidth"
+    system "go", "get", "github.com/pkg/errors"
+    system "go", "get", "github.com/sirupsen/logrus"
+    system "go", "get", "golang.org/x/crypto/ssh/terminal"
+    system "go", "get", "golang.org/x/oauth2"
+    system "go", "get", "golang.org/x/text/encoding"
+    system "go", "get", "google.golang.org/api/gmail/v1"
+    system "go", "get", "github.com/ThomasHabets/cmdg/cmd/cmdg"
+    system "go", "build", "github.com/ThomasHabets/cmdg/cmd/cmdg"
+    bin.install "./cmdg" => "cmdg"
+  end
+
+  test do
+    system "#{bin}/cmdg", "--help"
+  end
+end
