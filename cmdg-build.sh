fe74a2f2c5852738f298cacfd634efba691b524b
cutzenfriend <cutzenfriend@users.noreply.github.com>
2019-11-07 13:59:35 +0100

    Create cmdg-build.sh

---
+++ cmdg-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir go
+cd go
+export GOPATH=$(pwd)
+go get github.com/mattn/go-runewidth
+go get github.com/pkg/errors
+go get github.com/sirupsen/logrus
+go get golang.org/x/crypto/ssh/terminal
+go get golang.org/x/oauth2
+go get golang.org/x/text/encoding
+go get google.golang.org/api/gmail/v1
+go get github.com/ThomasHabets/cmdg
+go build github.com/ThomasHabets/cmdg/cmd/cmdg
