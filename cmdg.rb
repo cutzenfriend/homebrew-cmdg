c4fefec88f54595ffdb02f68fe6cbed5ce149e01
cutzenfriend <cutzenfriend@users.noreply.github.com>
2021-04-07 21:20:35 +0200

    Update cmdg.rb

--- cmdg.rb
+++ cmdg.rb
@@ -1,21 +1,11 @@
 class Cmdg < Formula
   desc "Gmail CLI - Copyright Thomas Habets thomas@habets.se 2019"
   homepage "https://github.com/ThomasHabets/cmdg"
-  url "https://github.com/cutzenfriend/homebrew-cmdg/releases/download/1.2.0/cmdg.rb"
-  sha256 "62f276fc355c1df9854936f1ee527d294cf9ad150729d3d957c5a20c2d78ab51"
-  version "1.2"
+  head "https://github.com/ThomasHabets/cmdg"
+  version "1.3"
   depends_on "go"
   def install
-    ENV["GOPATH"] = buildpath
-    system "go", "get", "github.com/mattn/go-runewidth"
-    system "go", "get", "github.com/pkg/errors"
-    system "go", "get", "github.com/sirupsen/logrus"
-    system "go", "get", "golang.org/x/crypto/ssh/terminal"
-    system "go", "get", "golang.org/x/oauth2"
-    system "go", "get", "golang.org/x/text/encoding"
-    system "go", "get", "google.golang.org/api/gmail/v1"
-    system "go", "get", "github.com/ThomasHabets/cmdg/cmd/cmdg"
-    system "go", "build", "github.com/ThomasHabets/cmdg/cmd/cmdg"
+    system "go", "build", "./cmd/cmdg"
     bin.install "./cmdg" => "cmdg"
   end
 
